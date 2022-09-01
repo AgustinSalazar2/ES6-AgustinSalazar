4f40f30d92acb9c9a837952f84e158131389497d
AgustinSalazar2 <agustinsssalazar@gmail.com>
2022-09-01 01:29:37 -0300

    Async-Await Promesas consultas API 01/09/22

--- 02_Promesas.js
+++ 02_Promesas.js
@@ -19,42 +19,54 @@
 > 8. Implementar bloques try catch para el manejo de errores. 
 */
 
-
 const retrasar = milisegundos => new Promise(resolve => setTimeout(resolve, milisegundos));
 
 // Función que retorna los datos de provincias
 const obtenerPcias = async () => {
     await retrasar(1800);
-    const consulta = await fetch('https://apis.datos.gob.ar/georef/api/provincias');
-    return consulta;
+    try {
+        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/provincias');
+        const jsonResp = await consulta.json();
+        return jsonResp;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Función que retorna los datos de departamentos
 const obtenerDptos = async () => {
     await retrasar(1391);
-
-    const consulta = await fetch('https://apis.datos.gob.ar/georef/api/departamentos');
-    return consulta;
+    try {
+        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/departamentos');
+        const jsonResp = await consulta.json();
+        return jsonResp;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // Función que retorna los datos de localidades
 const obtenerLocalidades = async () => {
     await retrasar(900);
-
-    const consulta = await fetch('https://apis.datos.gob.ar/georef/api/localidades');
-    return consulta;
+    try {
+        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/localidades');
+        const jsonResp = await consulta.json();
+        return jsonResp;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
 
 // Funcion para obtener todos los datos
 const consultarDatos = async () => {
-    const provincias = await obtenerPcias().then(resp=>resp.json());
-    const dptos = await obtenerDptos().then(resp=>resp.json());
-    const localidades = await obtenerLocalidades().then(resp=>resp.json());
+    const provincias = await obtenerPcias().then(resp=>resp.provincias).catch(err=>console.log(err));
+    const dptos = await obtenerDptos().then(resp=>resp.departamentos).catch(err=>console.log(err));
+    const localidades = await obtenerLocalidades().then(resp=>resp.localidades).catch(err=>console.log(err));
 
-    console.log(provincias.provincias);
-    console.log(dptos.departamentos);
-    console.log(localidades.localidades);
+    console.log(provincias);
+    console.log(dptos);
+    console.log(localidades);
 }
 consultarDatos();
