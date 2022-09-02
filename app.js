261bf2a424f3e195af20c20515a5e4f85c476f4f
AgustinSalazar2 <agustinsssalazar@gmail.com>
2022-09-02 13:58:31 -0300

    Async-await con funciones exportadas 02/09/22

--- app.js
+++ app.js
@@ -19,44 +19,8 @@
 > 8. Implementar bloques try catch para el manejo de errores. 
 */
 
-const retrasar = milisegundos => new Promise(resolve => setTimeout(resolve, milisegundos));
 
-// Función que retorna los datos de provincias
-const obtenerPcias = async () => {
-    await retrasar(1800);
-    try {
-        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/provincias');
-        const jsonResp = await consulta.json().then(resp=>resp.provincias);
-        return jsonResp;
-    } catch (error) {
-        return error
-    }
-}
-
-// Función que retorna los datos de departamentos
-const obtenerDptos = async () => {
-    await retrasar(1391);
-    try {
-        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/departamentos?max=529');
-        const jsonResp = await consulta.json().then(resp=>resp.departamentos);
-        return jsonResp;
-    } catch (error) {
-        return error
-    }
-}
-
-// Función que retorna los datos de localidades
-const obtenerLocalidades = async () => {
-    await retrasar(900);
-    try {
-        const consulta = await fetch('https://apis.datos.gob.ar/georef/api/localidades?max=4150');
-        const jsonResp = await consulta.json().then(resp=>resp.localidades);
-        return jsonResp;
-    } catch (error) {
-        return error
-    }
-}
-
+import {obtenerPcias, obtenerDptos, obtenerLocalidades} from "./libs/funciones-exportadas.js";
 
 
 // Funcion para obtener todos los datos
